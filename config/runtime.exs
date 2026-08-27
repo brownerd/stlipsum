214d0c8614167f94f6bbaed6d49aaebda458877b
gandhiShepard <benjamin@gandhishepard.com>
2026-08-27 15:34:26 -0500

    favicon ico added to runtime list ugh

--- config/runtime.exs
+++ config/runtime.exs
@@ -30,7 +30,7 @@ if config_env() == :dev do
       web_console_logger: true,
       patterns: [
         # Static assets, except user uploads
-        ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+        ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg|ico)$"E,
         # Router, Controllers, LiveViews and LiveComponents
         ~r"lib/stlipsum_web/router\.ex$"E,
         ~r"lib/stlipsum_web/(controllers|live|components)/.*\.(ex|heex)$"E
