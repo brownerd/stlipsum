ea0cf1842eecd5174c7473971bc3a3e379ba93cc
gandhiShepard <benjamin@gandhishepard.com>
2026-08-27 14:18:04 -0500

    update to fix connection error

--- config/runtime.exs
+++ config/runtime.exs
@@ -57,6 +57,7 @@ if config_env() == :prod do
 
   config :stlipsum, StlipsumWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: ["https://stlipsum.com", "https://www.stlipsum.com"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
